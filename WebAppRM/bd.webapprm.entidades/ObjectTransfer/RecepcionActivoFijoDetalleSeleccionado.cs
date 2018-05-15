20c97602749d4a551983a29dd731d6a04363cc77
Carlos Avila <carlos@atiss.une.cu>
2018-05-15 12:18:41 -0400

    Se comenzó la implementación de la vista parcial que va a contener todos los detalles de Activos Fijos.

---
+++ WebAppRM/bd.webapprm.entidades/ObjectTransfer/RecepcionActivoFijoDetalleSeleccionado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bd.webapprm.entidades.ObjectTransfer
+{
+    public class IdRecepcionActivoFijoDetalleSeleccionado
+    {
+        public int idRecepcionActivoFijoDetalle { get; set; }
+        public bool seleccionado { get; set; }
+    }
+
+    public class RecepcionActivoFijoDetalleSeleccionado
+    {
+        public RecepcionActivoFijoDetalle RecepcionActivoFijoDetalle { get; set; }
+        public bool Seleccionado { get; set; }
+    }
+}
